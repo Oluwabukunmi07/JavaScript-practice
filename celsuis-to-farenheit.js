c5d579ecb1bd78e2c61dd415aaddf62edc668ed4
Oluwabukunmi Akinbisola <preciousbukunmi07@gmail.com>
2025-09-27 14:46:36 +0100

    built a celsuis to farenheit converter

---
+++ celsuis-to-farenheit.js
@@ -0,0 +1,11 @@
+function convertCtoF (celsuis) {
+   let farenheit = celsuis * (9/5) + 32;
+   return farenheit;
+}
+
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(14));
+console.log(convertCtoF(0));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));

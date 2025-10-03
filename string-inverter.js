b88679c1867af66d2bd0b9ca0fabcd37d3027ff1
Oluwabukunmi Akinbisola <preciousbukunmi07@gmail.com>
2025-10-03 23:56:02 +0100

    string inverter

---
+++ string-inverter.js
@@ -0,0 +1,7 @@
+function reverseString (string) {
+  return string.split("").reverse().join("");
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("Howdy"));
+console.log(reverseString("Greetings from Earth"));

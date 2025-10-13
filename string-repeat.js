c4f4f5a8a1073f8426e1db1833f675167f40ccf4
Oluwabukunmi Akinbisola <preciousbukunmi07@gmail.com>
2025-10-13 15:43:21 +0100

    built a string repeating function

---
+++ string-repeat.js
@@ -0,0 +1,13 @@
+function repeatStringNumTimes (str, num) {
+  if (num <= 0) {
+    return ""
+  };
+
+  let newStr = "";
+  for(let i = 0; i < num; i++) {
+    newStr += str;
+  }
+  return newStr;
+}
+
+console.log(repeatStringNumTimes("hoe", 3));

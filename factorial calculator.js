78957be7eb7c469cd77e95d226571568ed14cc77
Oluwabukunmi Akinbisola <preciousbukunmi07@gmail.com>
2025-10-13 14:30:43 +0100

    built a factorail calculator

---
+++ factorial calculator.js
@@ -0,0 +1,15 @@
+let num = 6;
+
+function factorialCalculator (number) {
+
+  if (number === 0) {
+    return 1;
+  }
+
+}
+
+const factorial = factorialCalculator(num);
+
+const resultMsg =  `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg);
